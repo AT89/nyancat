31321013c508aaabfcca5e04fb9cf59db7439b9c
Anh Truong <anh.truong.07@cnu.edu>
2016-08-09 21:12:24 -0400

    loop, smaller mp3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-var audio = new Audio("/assets/sounds/yumyum.mp3");
-  audio.currentTime = 0;
-  audio.play();
+myAudio = new Audio('assets/sounds/yumyum.mp3');
+myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+myAudio.play();
