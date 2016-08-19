f32af397c3310b5b4097a2b1cfb12e41a27d6f21
Anh Truong <anh.truong.07@cnu.edu>
2016-08-19 16:56:03 -0400

    added plugin phaser-input

--- js/boot.js
+++ js/boot.js
@@ -1,6 +1,7 @@
 var bootState = {
   create: function()  {
     game.physics.startSystem(Phaser.Physics.ARCADE)
+    game.add.plugin(Fabrique.Plugins.InputField);
 
     game.state.start('load')
   }
--- js/score.js
+++ js/score.js
@@ -3,8 +3,14 @@
 //ENTER THE SCOREBOARD
 var scoreState = {
 
-  create: function() {
 
+
+  create: function() {
+    var your_score = game.add.text(game.world.width /2,
+      game.world.height * 2/10,
+       ''+this.score+'',
+      {font: '32px monospace', fill:'#fff'});
+      your_score.anchor.setTo(.5, 0);
 
     var startKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
     startKey.onDown.addOnce(this.start, this)
